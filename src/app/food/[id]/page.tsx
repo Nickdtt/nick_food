06080aa942111d90273d5e3938b441d596e0c239
Nícolas Figueiredo <nick@MacBook-Air-de-Nicolas.local>
2025-05-19 21:26:58 -0300

    correcao

--- src/app/food/[id]/page.tsx
+++ src/app/food/[id]/page.tsx
@@ -1,21 +1,31 @@
 import { foodList } from "@/app/bd";
 import Image from "next/image";
+import { notFound } from "next/navigation"
 
 
+type FoodPageProps = {
+    params: {
+        id: string
+    }
+}
 
 
-const foodDetail = ({params}: {params: {id:number}}) => {
+const foodDetail = ({params}: FoodPageProps) => {
 
-    const food = foodList.find((food) => food.id === params.id)
+    const food = foodList.find((food) => food.id.toString() === params.id)
+
+    if (!food) {
+        notFound()
+    }
 
     return (
         <div className="w-md flex flex-col justify-center items-center" >
             <div className="mt-20" >
-                <Image src={food!.imageUrl} alt={food!.name} width={350} height={350} />
+                <Image src={food.imageUrl} alt={food.name} width={350} height={350} />
             </div>
             <div>
-                <h1 className="text-3xl font-bold font-serif" >{food!.name}</h1>
-                <p className="text-sm text-muted-foreground" >{food!.bigDescription}</p>
+                <h1 className="text-3xl font-bold font-serif" >{food.name}</h1>
+                <p className="text-sm text-muted-foreground" >{food.bigDescription}</p>
             </div>
         </div>
     );
