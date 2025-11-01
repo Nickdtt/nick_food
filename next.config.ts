f9a8e8a3b9e3176c9feb644222ace74cdfb44240
Nicolas Figueiredo <mdf.nicolas@gmail.com>
2025-11-01 16:51:55 -0300

    fix(deploy): Temporarily disable Next.js image optimization for Render debugging

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone', // Adicionar esta linha
   images: {
+    unoptimized: true, // Adicionar esta linha para desativar a otimização
     remotePatterns: [
       {
         protocol: 'https',
